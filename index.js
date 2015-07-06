46ca906986b11a0703cfb269aff117eed7ad35fa
godsong <gaosong@hitour.cc>
2015-07-06 18:09:15 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-var Servex=module.exports = require('./lib/Servex');
-Servex.Utils=require('./lib/Utils');
-Servex.Promise=require('./lib/Promise');
-Servex.XParser=require('./lib/XParser');
-Servex.Model=require('./lib/Model');
-Servex.DataAdapter=require('./lib/DataAdapter');
-Servex.Config=require('./lib/Config');
-Servex.Service=require('./lib/Service');
+var Kodo=module.exports = require('./lib/Kodo.js');
+
